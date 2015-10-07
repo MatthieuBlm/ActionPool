282e3b80a1771a1c0aafcc73f84b6284b8f02ece
meyer <helene.meyer@univ-lille1.fr>
2015-10-07 11:49:31 +0200

    resource

---
+++ src/resource/Basket.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package resource;
+
+/**
+ * @author meyer
+ *
+ */
+public class Basket implements Resource {
+
+	/* (non-Javadoc)
+	 * @see resource.Resource#description()
+	 */
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
