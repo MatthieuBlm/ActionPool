05dc1c4370339a780fbaf432014b8c95ee6ce890
Bellamy Matthieu <bellamy@a16p5.fil.univ-lille1.fr>
2015-10-07 11:25:17 +0200

    Action + Scheduler

--- src/action/Action.java
+++ src/action/Action.java
@@ -1,9 +1,19 @@
 package action;
 
 public abstract class Action {
-
-	public Action() {
-		// TODO Auto-generated constructor stub
+	protected boolean isReady;
+	protected boolean isInitialized;
+	protected int totalStep;
+	
+	public Action(int stepToEnd) {
+		this.totalStep = stepToEnd;
 	}
-
+	
+	public boolean isReady(){
+		return isReady;
+	}
+	
+	public abstract boolean isInProgress();
+	public abstract boolean isFinished();
+	public abstract void doStep();
 }
